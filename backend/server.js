4252276f34ab1397366aad57ce5186e1de5365db
Sammys538 <sammy538.rodriguez@gmail.com>
2025-12-03 18:44:16 -0800

    Add basic routes for transaction and users

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ const port = 3000;
 
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
 // db.connect(err => {
 //     if(err) {
@@ -23,13 +23,45 @@ const db = mysql.createConnection({
 //     console.log("Connected to MySQL");
 // });
 
-
-
-
-
 app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
+
+//Transaction Routes
+app.post("/transaction", (req, res) => {
+    const {type, amount, description, date} = req.body;
+
+    console.log("transaction received: " + req.body);
+
+    res.json({message: "transaction received", data: req.body});
+})
+
+app.get("/transaction", (req, res) => {
+    const sample = [
+    { id: 1, type: "Income", amount: 200, description: "Service Money"},
+    { id: 2, type: "Expenses", amount: 20, description: "Purchases"}
+    ]
+
+    res.json(sample);
+})
+
+//Users Routes
+app.post("/users", (req, res) => {
+    const {name, email} = req.body;
+    console.log("New user", req.body);
+
+    res.json({message: "New User Added", data: req.body});
+
+})
+
+app.get("/users", (req, res) => {
+    const userSample = [
+        {id: 1, name: "Sam" , email: "dummy@email.com"},
+        {id: 2, name: "Jane Doe", email: "Test@test.com"}
+    ]
+
+    res.json(userSample);
+})
 
 
 
